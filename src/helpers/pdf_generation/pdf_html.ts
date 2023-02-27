7081f0043091db6a2533f0160aabd2f182ce4aa4
203420 <203420@ids.upchiapas.edu.mx>
2023-02-26 19:12:28 -0600

    PDF styles added

--- src/helpers/pdf_generation/pdf_html.ts
+++ src/helpers/pdf_generation/pdf_html.ts
@@ -4,8 +4,9 @@ export const createHTML = (data: Process): string => {
     return `<html>
                 <body>
                     <h1>Procesos de ${data.fase.nombre}</h1>
+                    <hr>
                     <div>
-                        <p><span>Nombre: </span>${data.name}</p>
+                        <p id="nombre"><span>Nombre: </span>${data.name}</p>
                         <p><span>Objetivo: </span>${data.object ? data.object : 'S/objetivo'}</p>
                         <p><span>Inidicadores y métricas: </span>${data.indicators ? data.indicators : 'S/indicators'}</p>
                     </div>
@@ -14,20 +15,20 @@ export const createHTML = (data: Process): string => {
                         <br>
                         <p>${data.description ? data.description : "S/Descripción"}</p>
                     </div>
-                    <div>
-                        <span>Diagrama de Flujo: </span>
+                    <div id="tituloD">
+                        <span id="DF">Diagrama de Flujo: </span>
                         <p><span>Responsable: </span>${data.responsable.name.toLocaleUpperCase()}</p>
                         <p><span>Url: </span><a href="${data.flujo_digram}">${data.flujo_digram ? data.flujo_digram : "S/Diagrama"}</a></p>
                     </div>
                     <div>
                         <p><span>Responsable: </span>${data.responsable.name.toLocaleUpperCase()}</p>
-                        <p><span>Participantes: </span>
+                        
                         
                         </p>
                         <p><span>Proceso Relacionado: </span>Ninguno</p>
                     </div>
                     <div>
-                    <p><span>Evidencias de entrada</span>
+                    <p id="tituloEv"><span >Evidencias de entrada</span>
                     ${
                         data.evidencia_entrada.length > 0 ?
                             data.evidencia_entrada.map((e, i) => 
@@ -35,17 +36,56 @@ export const createHTML = (data: Process): string => {
                             ).join('') : 'S/Evidencias'
                     }
                     </p>
-                    <p><span>Evidencias de salida</span>
+                    <p id="tituloEv"><span >Evidencias de salida</span>
                     ${
                         data.evidencia_salida.length > 0 ?
-                        data.evidencia_entrada.map((e, i) => 
-                        `<p><span>Evidencia de entrada${i + 1}: </span><a href="${e.url}">${e.url}</a></p>`
+                        data.evidencia_salida.map((e, i) => 
+                        `<p><span>Evidencia de salida${i + 1}: </span><a href="${e.url}">${e.url}</a></p>`
                         ).join('') : 'S/Evidencias'
                     }
                     </p>
                     </div>
                     <p><span>Frecuencia: </span> Cada vez que se desee iniciar la fase</p>
-                    
+                    <style>
+                        html {
+                            font-family: sans-serif;
+                            color: #3E3E3E
+                        }
+                        h1 {
+                            font-size: 2.5em;
+                            font-weight: 900;
+                        }
+                        hr {
+                            border: none;
+                            background: #3E3E3E;
+                            height: 0.35em;
+                            margin-bottom: 2em;
+                        }
+                        span {
+                            font-weight: 700;
+                            font-size: 1.2em;
+                        }
+                        #nombre {
+                            font-weight: 700;
+                            font-size: 1.4em;
+                            margin-bottom: 1.5em;
+                        }
+                        p {
+                            font-size: 1.1em;
+                        }
+                        #tituloEv{
+                            margin-top: 2.5em;
+                        }
+                        #tituloEv span{
+                            font-size: 1.4em;
+                        }
+                        #tituloD{
+                            margin-top: 2.25em;
+                        }
+                        #tituloD #DF{
+                            font-size: 1.4em;
+                        }
+                    </style>
                 </body>
             </html>`;
 }
